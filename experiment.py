b6d2d7f5e04bb8188920642e28d5b39f16bba3c1
Al Gharakhanian <algh@comcast.net>
2020-04-07 22:40:47 -0700

    first commit after preprocessing

---
+++ experiment.py
@@ -0,0 +1,165 @@
+import pandas as pd
+import numpy as np
+
+
+from sklearn.preprocessing import StandardScaler,MinMaxScaler,LabelEncoder
+le = LabelEncoder()
+
+a = [[1,2],[3.4]]
+for i in range(len(a)):
+    print (a[i])
+
+
+exit()
+
+dates = pd.date_range('20130101', periods=6)
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
+print(df)
+df.drop(columns=['A'],inplace=True)
+print (df)
+exit()
+print(type(df['A']))
+coladf = df[['A']]
+print(type(df[['A']]))
+df.loc[:,'A'] = 18
+print  (df)
+df['A']=df['B']
+print (type(df['A']))
+print (df.loc[:,'A'])
+print (type(df.loc[:,'A']))
+#a = np.array([[1],[2],[3],[4],[4],[5]])
+a = np.array(range(6))
+print(a)
+df.loc[:,'A'] = a
+print(df.info())
+
+exit()
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+
+dtext = ['20160101']
+tt = dtext[0]
+yr = int(tt [:4])
+mo = int (tt[4:6])
+day = int (tt[6:])
+
+def func(a):
+    yr = int(a[:4])
+    mo = int(a[4:6])
+    day = int(a[6:])
+    return yr*365+mo*12+day
+
+
+
+
+
+df = pd.DataFrame({'col':['11921123']})
+print(df.head())
+#df[['A']].apply(lambda x: func(df['A']))
+df=df['col'].apply(func)
+print(df.head())
+
+'''
+df = pd.DataFrame({'date': ['19910101','19921204','19930104']})
+dates = pd.to_datetime(df['date'], format='%Y%m%d', errors='ignore')
+df.info()
+
+print (df)
+datesnp=dates.values
+datesnp=datesnp.reshape(len(datesnp), 1)
+
+scaler = scaler.fit(datesnp)
+normalized = scaler.transform(datesnp)
+
+#(aaa.time).total_seconds()
+#print(aaa)
+exit()
+
+
+mf = pd.DataFrame({'col1':[1,2,1,3,4,2,12,33,33]})
+mfn = mf.to_numpy()
+colname = mf.columns
+mm.fit(mf[colname])
+#mm.fit(mf[['col1']])
+mf[colname]=mm.transform(mf[colname])
+print ('hello')
+exit()
+
+
+
+
+
+#le.fit(mf)
+colname=mf.columns[0]
+le.fit(mf[colname])
+#le.fit(mf['col1'])
+print (le.classes_)
+tolist = mf['col1'].tolist()
+#tolist = mf['col1'].tolist()
+print (type(le.transform(tolist)))
+print (le.transform(tolist))
+mf['col1'] = le.transform(tolist)
+print (mf)
+
+print ('hello')
+
+
+print ('print (myframe)  ', myframe)
+print('-----------------------------------------------------')
+
+myframe['col1'] = le.fit(myframe['col1'])
+print ('myframe.columns[0]')
+print (myframe.columns[0])
+print('-----------------------------------------------------')
+
+print ('(type(myframe.columns[0]))')
+print (type(myframe.columns[0]))
+print('-----------------------------------------------------')
+
+print ('le.classes')
+print (le.classes_)
+print('-----------------------------------------------------')
+
+print ('type(myframe)')
+print (type(myframe))
+print('-----------------------------------------------------')
+print ('myframe.shape')
+print (myframe.shape)
+print('-----------------------------------------------------')
+print ('myframe.info')
+print (myframe.info)
+print('-----------------------------------------------------')
+print('le.transform([1,4,33])')
+print(le.transform([1,4,33]))
+print('-----------------------------------------------------')
+print('type((myframe.loc[:,[col1]].to_numpy()))')
+print(type((myframe.loc[:,['col1']].to_numpy())))
+print('-----------------------------------------------------')
+#aaa= myframe.loc[:,['col1']].to_numpy()
+
+ccc = myframe.loc[:,['col1']].to_numpy()
+print ('ccc = myframe.loc[:,[col1]].to_numpy()')
+
+xxx = myframe.loc[:,'col1'].to_numpy()
+
+print (xxx,'  ', 'xxx is')
+
+fff = list(myframe.loc[:,['col1']])
+aaa = myframe['col1']
+bbb = myframe.loc[:,'col1']
+ddd =  list (ccc)
+eee = [1,2,33]
+print (type(aaa))
+print (type(bbb))
+print (type(ccc))
+print (fff)
+print (type(fff))
+
+
+
+print (le.transform(fff))
+#myframe.loc[:,['col']] = le.transform(myframe['col1'].to_numpy())
+'''
+
+
+#print (myframe.head())
---
+++ standardize.py
@@ -0,0 +1,322 @@
+#import argparse
+#import json
+import os
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler,MinMaxScaler,LabelEncoder
+import numpy as np
+std_scaler = StandardScaler()
+mm_scaler = MinMaxScaler()
+lbl_encoder = LabelEncoder()
+
+colnames =          ['MEMBERSHIP_NUMBER',
+                    'MEMBERSHIP_TERM_YEARS',
+                    'ANNUAL_FEES',
+                    'MEMBER_MARITAL_STATUS',
+                    'MEMBER_GENDER',
+                    'MEMBER_ANNUAL_INCOME',
+                    'MEMBER_OCCUPATION_CD',
+                    'MEMBERSHIP_PACKAGE',
+                    'MEMBER_AGE_AT_ISSUE',
+                    'ADDITIONAL_MEMBERS',
+                    'PAYMENT_MODE',
+                    'AGENT_CODE',
+                    'START_DATE',
+                    'END_DATE',
+                    'MEMBERSHIP_STATUS']
+
+newcolnames=['MEMBERSHIP_STATUS',
+              'MEMBERSHIP_TERM_YEARS_MINMAX_SC',
+             'MEMBERSHIP_TERM_YEARS_STD_SC',
+             'ANNUAL_FEES_MINMAX_SC',
+             'ANNUAL_FEES_STD_SC',
+             'MEMBER_MARITAL_STATUS_MOST_CAT',
+             'MEMBER_GENDER_MOST_CAT',
+             'MEMBER_ANNUAL_INCOME_AVG_MINMAX_SC',
+              'MEMBER_ANNUAL_INCOME_AVG_STD_SC',
+             'MEMBER_OCCUPATION_CD_CAT',
+             'MEMBERSHIP_PACKAGE_CAT',
+             'MEMBER_AGE_AT_ISSUE_MINMAX_SC',
+             'ADDITIONAL_MEMBERS_CAT',
+             'PAYMENT_MODE_CAT',
+             'START_DATE_MINMAX_SC',
+             'END_DATE_MINMAX_SC',
+             'MEMBERSHIP_DUR_MINMAX_SC']
+
+
+ds_len = 'Long'
+ds_type =   'Train'
+is_short = True if ds_len == 'Short' else False
+is_long = not is_short
+is_tr   = True if ds_type == 'Train' else False
+is_te   = not is_tr
+
+InFilenames       = ['./dataset/club_churn_test_short.csv','./dataset/club_churn_test.csv','./dataset/club_churn_train_short.csv','./dataset/club_churn_train.csv',]
+OutFilenames       = ['./dataset/club_churn_test_short_std.csv','./dataset/club_churn_test_std.csv','./dataset/club_churn_train_short_std.csv','./dataset/club_churn_train_std.csv']
+ptr = 2*is_tr + is_long
+dataset_in      = InFilenames [ptr]
+
+dataset_out      = OutFilenames [ptr]
+
+print ('hello')
+
+###### utilities
+def clear_old_outputs (file):
+    if os.path.isfile(file):
+        os.remove(file)
+        print("Old file cleared")
+    else:
+        print("Directory Clear")
+    return
+
+def fill_blanks(df,item):
+    return df.fillna (item)
+
+
+def date2int(a):
+    astr = str(round(a))
+    yr = int(astr[:4])
+    mo = int(astr[4:6])
+    day = int(astr[6:])
+    return yr*365+mo*30+day
+
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+def append_r(col,pd_o):
+    pd_out = pd.concat([pd_o,col],axis=1)
+    return pd_out
+
+def append_l(col,pd_o):
+    pd_out = pd.concat([col,pd_o],axis=1)
+    return pd_out
+
+#imputes
+def most_often_imp(col):
+    mode = col.mode().as_matrix()[0,0]
+    col.fillna(value=mode,inplace=True)
+    return col
+
+def avg_imp(col):
+    col.fillna(col.mean(), inplace=True)
+    return col
+
+#scalers
+def minmax_scaler(col):
+    colname = col.columns[0]
+    colvals= col[colname].values
+    colvals = colvals.reshape(len(colvals), 1)
+    mm_scaler.fit(colvals)
+    col.loc[:,colname] = mm_scaler.transform(colvals)
+    return col
+
+#functions
+def std_scale(col):
+    colname = col.columns
+    std_scaler.fit(col[colname])
+    col[colname] = std_scaler.transform(col[colname])
+    return col
+
+def categorize(col):
+    colname = col.columns[0]
+    col_list= col[colname].tolist()
+    lbl_encoder.fit(col_list)
+    print(lbl_encoder.classes_)
+    pd_o = pd.DataFrame({colname:lbl_encoder.transform(col_list)})
+    return pd_o
+
+def mem_status_cat(pd_i,pd_o):
+    src_col = 'MEMBERSHIP_STATUS'
+    tgt_col =   'MEMBERSHIP_STATUS'
+    pd_o [tgt_col] = pd_i [src_col]
+    pd_o[tgt_col] = categorize(pd_o [[tgt_col]])
+    return pd_o
+#-----------------------------------------------------------
+def mem_term_yrs_mm_sc(pd_i,pd_o):
+
+
+    src_col = 'MEMBERSHIP_TERM_YEARS'
+    tgt_col = 'MEMBERSHIP_TERM_YEARS_MINMAX_SC'
+    pd_o[tgt_col]=minmax_scaler(pd_i [[src_col]])
+    return pd_o
+
+def mem_term_yrs_std_sc(pd_i,pd_o):
+
+    src_col = 'MEMBERSHIP_TERM_YEARS'
+    tgt_col = 'MEMBERSHIP_TERM_YEARS_STD_SC'
+    pd_o[tgt_col] = std_scale(pd_i[[src_col]])
+    return pd_o
+
+def ann_fee_mm_sc(pd_i,pd_o):
+    src_col = 'ANNUAL_FEES'
+    tgt_col = 'ANNUAL_FEES_MINMAX_SC'
+    pd_o[tgt_col] = minmax_scaler(pd_i[[src_col]])
+    return pd_o
+
+def ann_fee_std_sc(pd_i,pd_o):
+    src_col = 'ANNUAL_FEES'
+    tgt_col = 'ANNUAL_FEES_STD_SC'
+    pd_o[tgt_col] = std_scale(pd_i[[src_col]])
+    return pd_o
+
+def mem_marstat_most_cat(pd_i,pd_o):
+    src_col = 'MEMBER_MARITAL_STATUS'
+    tgt_col = 'MEMBER_MARITAL_STATUS_MOST_CAT'
+    imputed_pd = most_often_imp(pd_i[[src_col]])
+    pd_o[tgt_col]= categorize(imputed_pd)
+    return pd_o
+
+def mem_gen_most_cat(pd_i,pd_o):
+    src_col = 'MEMBER_GENDER'
+    tgt_col = 'MEMBER_GENDER_MOST_CAT'
+    imputed_pd = most_often_imp(pd_i[[src_col]])
+    pd_o[tgt_col] = categorize(imputed_pd)
+    return pd_o
+
+def mem_inc_avg_mm_sc(pd_i,pd_o):
+    src_col = 'MEMBER_ANNUAL_INCOME'
+    tgt_col = 'MEMBER_ANNUAL_INCOME_AVG_MINMAX_SC'
+    imputed_pd = avg_imp(pd_i[[src_col]])
+    pd_o[tgt_col] = minmax_scaler(imputed_pd)
+    return pd_o
+
+def mem_inc_avg_std_sc(pd_i,pd_o):
+    src_col = 'MEMBER_ANNUAL_INCOME'
+    tgt_col = 'MEMBER_ANNUAL_INCOME_AVG_STD_SC'
+    imputed_pd = avg_imp(pd_i[[src_col]])
+    pd_o[tgt_col] = std_scale(imputed_pd[[src_col]])
+    return pd_o
+
+def mem_occ_cat(pd_i,pd_o):
+    src_col = 'MEMBER_OCCUPATION_CD'
+    tgt_col = 'MEMBER_OCCUPATION_CD_CAT'
+    imputed_pd = most_often_imp(pd_i[[src_col]])
+    pd_o[tgt_col] = categorize(imputed_pd[[src_col]])
+    return pd_o
+
+def mem_pkg_cat(pd_i,pd_o):
+    src_col = 'MEMBERSHIP_PACKAGE'
+    tgt_col = 'MEMBERSHIP_PACKAGE_CAT'
+    pd_o[tgt_col] = categorize(pd_i[[src_col]])
+    return pd_o
+
+def mem_age_mm_sc(pd_i,pd_o):
+    src_col = 'MEMBER_AGE_AT_ISSUE'
+    tgt_col = 'MEMBER_AGE_AT_ISSUE_MINMAX_SC'
+    pd_o[tgt_col] = minmax_scaler(pd_i[[src_col]])
+    return pd_o
+
+def mem_addmem_mm_sc(pd_i,pd_o):
+    src_col = 'ADDITIONAL_MEMBERS'
+    tgt_col = 'ADDITIONAL_MEMBERS_CAT'
+    pd_o[tgt_col] = categorize(pd_i[[src_col]])
+    return pd_o
+
+def mem_paymode_cat(pd_i,pd_o):
+    src_col = 'PAYMENT_MODE'
+    tgt_col = 'PAYMENT_MODE_CAT'
+    pd_o[tgt_col] = categorize(pd_i[[src_col]])
+    return pd_o
+
+def mem_stdate_mm_sc(pd_i,pd_o):
+    src_col = 'START_DATE'
+    tgt_col = 'START_DATE_MINMAX_SC'
+    src_col_end = 'END_DATE'
+    tgt_col_dur =  'MEMBERSHIP_DUR_MINMAX_SC'
+
+    max_date = pd_i[src_col_end].max()
+    pd_i[src_col_end].fillna(value=max_date, inplace=True)
+    end_datesn = pd_i[src_col_end].apply(date2int)
+    st_datesn = pd_i[src_col].apply (date2int)
+
+
+
+    mem_dur = end_datesn - st_datesn
+
+    st_datesnp = st_datesn.values
+    st_datesnp = st_datesnp.reshape(len(st_datesnp), 1)
+    scaler = mm_scaler.fit(st_datesnp)
+    pd_o[tgt_col] = mm_scaler.transform(st_datesnp)
+
+    mem_durnp = mem_dur.values
+    mem_durp = mem_durnp.reshape(len(mem_durnp), 1)
+    scaler2 = mm_scaler.fit(mem_durp)
+    pd_o[tgt_col_dur] = mm_scaler.transform(mem_durp)
+    return pd_o
+
+def mem_endt_mm_sc(pd_i,pd_o):
+    src_col = 'END_DATE'
+    tgt_col = 'END_DATE_MINMAX_SC'
+    pd_o.drop(columns=[tgt_col],inplace=True)
+    return pd_o
+
+def mem_dur_mm_sc(pd_i,pd_o):
+    src_col = 'MEMBER_OCCUPATION_CD'
+    tgt_col = 'MEMBER_OCCUPATION_CD_CAT'
+    return pd_o
+
+
+def pre_processor(pin,cn,pout):
+    if cn==0:
+        pout = mem_status_cat (pin,pout)
+    if cn == 1:
+        pout = mem_term_yrs_mm_sc(pin,pout) #
+    elif cn== 2:
+        pout = mem_term_yrs_std_sc(pin,pout) #
+    elif cn== 3:
+        pout = ann_fee_mm_sc(pin,pout) #
+    elif cn== 4:
+        pout = ann_fee_std_sc(pin,pout) #
+    elif cn== 5:
+        pout = mem_marstat_most_cat(pin,pout) #
+    elif cn== 6:
+        pout = mem_gen_most_cat(pin,pout) #
+    elif cn== 7:
+        pout = mem_inc_avg_mm_sc(pin,pout) #
+    elif cn== 8:
+        pout = mem_inc_avg_std_sc(pin,pout) #
+    elif cn== 9:
+        pout = mem_occ_cat(pin,pout) #
+    elif cn == 10:
+        pout = mem_pkg_cat(pin, pout) #
+    if cn == 11:
+        pout = mem_age_mm_sc(pin,pout) #
+    elif cn == 12:
+        pout = mem_addmem_mm_sc(pin,pout) #
+    elif cn == 13:
+        pout = mem_paymode_cat(pin,pout)
+    elif cn== 14:
+        pout = mem_stdate_mm_sc(pin,pout)
+    elif cn== 15:
+        pout=   mem_endt_mm_sc (pin,pout)
+    elif cn == 16:
+        pout = mem_dur_mm_sc(pin, pout)
+    return pout
+
+###### retrieve input dataset
+clear_old_outputs(dataset_out)
+print(dataset_in)
+pd_dsetin =pd.read_csv(dataset_in)
+print (pd_dsetin.dtypes)
+nrows=pd_dsetin.shape[0]
+ncols=pd_dsetin.shape[1]
+n_newcols:len(newcolnames)
+pd_dsetout =pd.DataFrame(index=np.arange(nrows),columns=newcolnames)
+print (ncols, '   ', nrows)
+
+##### Main Loop
+for coln,col in enumerate(newcolnames):
+   print (coln,col)
+   p_out = pre_processor(pd_dsetin,coln,pd_dsetout)
+aaa = dataset_out
+p_out.to_csv(dataset_out,index=False)
+exit()
+
+
+
+
+
+
+
+
+
+
+
