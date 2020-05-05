b0fcd0bc4f570e76de15531863d854a2ab7b8d4f
Al Gharakhanian <algh@comcast.net>
2020-05-05 10:45:00 -0700

    troubleshooting git

--- args_pg.py
+++ args_pg.py
@@ -1,32 +1,30 @@
 import argparse
 ####setup parameters
 def parse_args():
-
     parser = argparse.ArgumentParser()
-    #parser.add_argument("--src_file", type=str, default=
+    parser.add_argument("--src_file", type=str, default=
                         './dataset/club_churn_source.csv')
-    #parser.add_argument("--shuffle", type=int, default=5)
-    #parser.add_argument("--plt_learn_cv", type=bool, default=True)
-    #parser.add_argument("--trte_split", type=float, default=0.2)
-    #parser.add_argument("--epochs", type=int, default=5)
-    #parser.add_argument("--Kfolds", type=int, default=1)
-
-    #baseparam=dict(objective='binary:logistic')
-    #baseparam.update(dict(n_estimators=20))
-    #baseparam.update(dict(learning_rate=0.1))
-    #baseparam.update(dict(scale_pos_weight=1))
-    #baseparam.update(dict(max_depth=4))
-    #baseparam.update(dict(min_child_weight=5))
-    #baseparam.update(dict(gamma=0.3))
-    #baseparam.update(dict(subsample=0.6))
-    #baseparam.update(dict(colsample_bytree=0.7))
-    #parser.add_argument("--base_param", type=dict, default=baseparam )
-    #parser.add_argument("--max_eval", type=int, default=3)
-    #parser.add_argument("--max_eval", type=int, default=160)
-    #parser.add_argument
+    '''
+    parser.add_argument("--shuffle", type=int, default=5)
+    parser.add_argument("--plt_learn_cv", type=bool, default=True)
+    parser.add_argument("--trte_split", type=float, default=0.2)
+    parser.add_argument("--epochs", type=int, default=5)
+    parser.add_argument("--Kfolds", type=int, default=1)
+    baseparam=dict(objective='binary:logistic')
+    baseparam.update(dict(n_estimators=20))
+    baseparam.update(dict(learning_rate=0.1))
+    baseparam.update(dict(scale_pos_weight=1))
+    baseparam.update(dict(max_depth=4))
+    baseparam.update(dict(min_child_weight=5))
+    baseparam.update(dict(gamma=0.3))
+    baseparam.update(dict(subsample=0.6))
+    baseparam.update(dict(colsample_bytree=0.7))
+    parser.add_argument("--base_param", type=dict, default=baseparam )
+    parser.add_argument("--max_eval", type=int, default=3)
+    parser.add_argument("--max_eval", type=int, default=160)
+    '''
     args=argparse
-    return parse_args()
-
+    return args
 
 '''
 Notes
